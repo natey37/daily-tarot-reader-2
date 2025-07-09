441255f3957e62e994e43d4e7957061d7edf12c3
Nathan Brickett <nathanbrickett@gmail.com>
2025-07-08 18:28:37 -0700

    fix: add trust proxy and update keyGenerator

--- server/src/index.js
+++ server/src/index.js
@@ -1,34 +1,42 @@
-import './loadEnv.js';
+import "./loadEnv.js";
 
 import express from "express";
 import cors from "cors";
 import interpretRouter from "./routes/interpret.js";
-import rateLimit from 'express-rate-limit'
+import rateLimit from "express-rate-limit";
 
 const app = express();
+app.set("trust proxy", true);
+
 app.use(cors());
 app.use(express.json());
 
-const WHITELISTED_IP = process.env.WHITELISTED_IP || '127.0.0.1';
+const WHITELISTED_IP = process.env.WHITELISTED_IP || "127.0.0.1";
 
 const ipLimiter = rateLimit({
-    windowMs: 60 * 1000, // 1 minute
-    max: 1,
-    keyGenerator: (req) => {
-      const ip = req.ip === '::1' ? '127.0.0.1' : req.ip;
-      console.log('Incoming request IP:', ip);
-      return ip;
-    },
-    skip: (req) => {
-      const ip = req.ip === '::1' ? '127.0.0.1' : req.ip;
-      return ip === WHITELISTED_IP;
-    },
-    handler: (req, res) => {
-      res.status(429).json({ message: 'Too many requests. Please wait a minute.' });
-    },
-  });
+  windowMs: 60 * 1000, // 1 minute
+  max: 1,
+  keyGenerator: (req) => {
+    const xForwardedFor = req.headers["x-forwarded-for"];
+    const ip = req.ip === "::1" ? "127.0.0.1" : req.ip;
 
-app.use('/api/interpret', ipLimiter, interpretRouter);
+    console.log("Incoming request IP:", ip);
+    console.log("X-Forwarded-For header:", xForwardedFor);
+
+    return ip;
+  },
+  skip: (req) => {
+    const ip = req.ip === "::1" ? "127.0.0.1" : req.ip;
+    return ip === WHITELISTED_IP;
+  },
+  handler: (req, res) => {
+    res
+      .status(429)
+      .json({ message: "Too many requests. Please wait a minute." });
+  },
+});
+
+app.use("/api/interpret", ipLimiter, interpretRouter);
 
 const PORT = process.env.PORT || 4000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
